fe5b8930c5a542de55128af93bffafa772b8a007
PielordZZ <zzpieme@gmail.com>
2022-02-05 13:30:44 -0500

    Deleted obsolete file and wrote basic analysis

--- Earnings_Over_Time/Time_Functions.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def addYear(largeDataFrame):
-
-    largeDataFrame['Release Year'] = int(str((largeDataFrame['Title'])).split('(')[1].split(')')[0])
-
-    return largeDataFrame
-
